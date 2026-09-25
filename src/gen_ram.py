f03e2c1c1f7a0693ac510c454c117331ed930dda
dalinac <chabane.dalina2021@gmail.com>
2026-09-25 14:45:49 +0200

    modif : passage en interger

--- src/gen_ram.py
+++ src/gen_ram.py
@@ -4,13 +4,14 @@ def gen_sin(n: int):
     return (np.sin(np.linspace(0, 2*np.pi, n+1)[:-1]) + 1)/2
 
 @np.vectorize
-def fmt_float_hex(x, nbits=14):
-    x = int(np.rint(x*(2**nbits-1)))
-    return hex(x)[2:].rjust(int(np.ceil(nbits/4)), '0')
+def fmt_float_int(x, nbits=14):
+    return str(int(np.rint(x*(2**nbits-1))))
 
-def gen_vhdl(n, nbits=14, name='RAM', type_name='ram_t'):
-    r = f"type {type_name} is array (0 to {n-1}) of  std_logic_vector ({nbits-1} downto 0);\n"
-    r += f"signal {name}: {type_name} :=({nbits}x\"{f'",{nbits}x"'.join(fmt_float_hex(gen_sin(n), nbits))}\");\n"
+def gen_vhdl(n, nbits=14, name='RAM', type_name='ram_t', per_line=16):
+    vals = fmt_float_int(gen_sin(n), nbits)
+    lines = [', '.join(vals[i:i+per_line]) for i in range(0, n, per_line)]
+    r = f"type {type_name} is array (0 to {n-1}) of integer range 0 to {2**nbits-1};\n"
+    r += f"signal {name}: {type_name} :=(\n\t" + ",\n\t".join(lines) + ");\n"
     return r
 
 if __name__ == '__main__':
